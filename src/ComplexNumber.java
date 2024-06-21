33262f30f4288625ae1ec342785fd3b1a13596f8
Tu Ngo <tngo@softel.vn>
2024-06-21 17:49:17 +0700

    Update complex number

--- src/ComplexNumber.java
+++ src/ComplexNumber.java
@@ -1,5 +1,28 @@
-public class ComplexNumber {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class ComplexNumber {
+
+    double real, img;
+
+    ComplexNumber(double r, double i){
+        this.real = r;
+        this.img = i;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập phần thực số a");
+
+
+        ComplexNumber a = new ComplexNumber(1, 2);
+        ComplexNumber b = new ComplexNumber(3, 4);
+        ComplexNumber ComplexNumberResult = sum(a, b);
+        System.out.printf("Kết quả là: "+ ComplexNumberResult.real+" + "+ ComplexNumberResult.img +"i");
+    }
+
+    public static ComplexNumber sum(ComplexNumber a, ComplexNumber b) {
+        return new ComplexNumber(a.real + b.real, a.img + b.img);
+    }
+
+
 }
